8b18b641be63d6327ac7f750c5d14fe5ed201f44
Cole Lyman <cole@colelyman.com>
2015-12-28 17:26:05 -0500

    Created basic test using angularFire

--- assets/js/ht.js
+++ assets/js/ht.js
@@ -1,3 +1,9 @@
-var firebase = new Firebase("https://provo166ht.firebaseio.com/");
+var app = angular.module('htApp', ['firebase']);
 
-console.log(firebase);
+app.controller('ctrl', function($scope, $firbaseAuth) {
+  var ref = new Firebase("https://provo166ht.firebaseio.com/");
+
+  var data = $firebaseObject(ref); // this is async
+
+  data.$bindTo($scope, 'data');
+});
