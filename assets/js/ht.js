85a151d5243694d8f99a29caac45fa58eb17581b
Cole Lyman <cole@colelyman.com>
2015-12-29 23:38:58 +0000

    Can add hometeachers with associated hometeachees, can also reportls Need to make reporting individual, rather than based on a family

--- assets/js/ht.js
+++ assets/js/ht.js
@@ -1,11 +1,33 @@
 var app = angular.module('htApp', ['firebase']);
 
-app.controller('ctrl', function($scope, $firebaseObject) {
+app.controller('ctrl', function($scope, $firebaseArray) {
   var ref = new Firebase("https://provo166ht.firebaseio.com/");
 
-  var data = $firebaseObject(ref); // this is async
+  $scope.data = $firebaseArray(ref); // this is sync
 
-  data.$bindTo($scope, 'data');
+  //data.$bindTo($scope, 'data');
+
+  $scope.monthSelect = null;
+
+  $scope.addGroup = function() {
+    $scope.group.report = [
+      {month: 'January', visited: false, concerns: ''},
+      {month: 'February', visited: false, concerns: ''},
+      {month: 'March', visited: false, concerns: ''},
+      {month: 'April', visited: false, concerns: ''}
+    ];
+    $scope.data.$add($scope.group);
+    $scope.group = null;
+  };
+
+  $scope.updateConcerns = function(group) {
+    $scope.data.$save(group);
+  };
+
+  $scope.updateVisitStatus = function(group, visited) {
+    console.log(JSON.stringify(group));
+    console.log(visited);
+  }
 });
 
 app.config(function($interpolateProvider) {
